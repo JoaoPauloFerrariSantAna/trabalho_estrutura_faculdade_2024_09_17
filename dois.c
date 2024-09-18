378dd37644ec893a8f02eaa99c9041ee7b4197bd
Leonardo Perin Andreozi <155500125+Perinzinho@users.noreply.github.com>
2024-09-18 19:36:33 -0300

    Colocando if para cima por conta de bug

--- dois.c
+++ dois.c
@@ -3,16 +3,14 @@
 
 int potencia(int a,int b){//Função para calcular potencia, recebe 2 parâmetros a e b
     int pot=1;//Declaração da variável pot que vai receber a função pow
+    if(b==0)//Na matemática qualquer número elevado a 0 é igual a 1
+    return 1;
+    if(b==1)//Na matemática qualquer número elevado a 1 é igual a ele mesmo
+        return a;
     for(int i=0; i<b; i++){//Realiza um laço de repetição aonde a vai ser multiplicado pelo valor que b receber
         pot= pot*a;
     }
     return pot;
-    if(b==0)//Na matemática qualquer número elevado a 0 é igual a 1
-        return 1;
-    if(b==1)//Na matemática qualquer número elevado a 1 é igual a ele mesmo
-        return a;
-
-
 }
 
 int main(){//Função Principal para a realização da conta
